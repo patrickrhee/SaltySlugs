e7a8be78c49db635b05a2e61a4f5f4e2a166cdbe
Patrick Rhee <psr15@pitt.edu>
2020-01-11 18:12:37 -0500

    Added crawl animation to slug player sprite whenever it moves. Also changed the direction in which slug moves to follow touch location

--- Salty Slugs/GameScene.swift
+++ Salty Slugs/GameScene.swift
@@ -16,73 +16,83 @@ class GameScene: SKScene{
     private var spinnyNode : SKShapeNode?
     
     // Create our player slug character
-    private let player = SKSpriteNode(imageNamed: "player_slug");
+    private var player = SKSpriteNode();
+    private var playerWalkFrames : [SKTexture] = [];
+    
+    // Keep track of last touch position for player slug to go to.
+    private var touchPos : CGPoint?
+    
+    // Keep track of if the slug animation is player
+    private var walkAnimOn = false;
+    
+    // Function to create slug player sprite and put it in the SKScene
+    func buildPlayerSlug()
+    {
+        // Build a texture atlas with our .atlas folder as input, then copy it
+        // into the temp walkFrames SKTexture array, which is then copied into
+        // our global SKTexture array variable.
+        let playerSlugWalkAnimatedAtlas = SKTextureAtlas(named: "player_walk");
+        var walkFrames: [SKTexture] = [];
+        
+        // For every SKTexture in the atlas, append it to our walkFrames array
+        let numImages = playerSlugWalkAnimatedAtlas.textureNames.count;
+        for i in 0...(numImages-1)
+        {
+            let playerWalkTextureName = "player_walk_\(i)"
+            
+            walkFrames.append(playerSlugWalkAnimatedAtlas.textureNamed(playerWalkTextureName))
+        }
+        
+        // Global array is equal to local temp array.
+        playerWalkFrames = walkFrames;
+        
+        // Set the player sprite with first texture/image.
+        let firstFrameTexture = playerWalkFrames[0];
+        player = SKSpriteNode(texture: firstFrameTexture);
+        player.position = CGPoint(x: 0, y: 0); // Set sprite to cords (0,0)
+        player.size = CGSize(width: 400, height: 280)
+        addChild(player); // Add player sprite to the scene
+        
+    }
+    
+    // Function to animate player slug
+    func animatePlayerSlug()
+    {
+        player.run(SKAction.repeatForever(SKAction.animate(with: playerWalkFrames, timePerFrame: 0.15, resize:false, restore: true)), withKey:"walkingInPlaceSlug");
+        // The withKey attribute identifies this SKAction with a name, so
+        // if this func is called again, we just restart this action, not create
+        // a new one.
+    }
     
     override func didMove(to view: SKView) {
         // Create a background color for the scene.
         backgroundColor = SKColor.init(red: 185/255, green: 235/255, blue: 145/255, alpha: 1.0);
         
-        // set up player sprite
-        player.position = CGPoint(x:0, y: 0);
-        player.size = CGSize(width: 360, height: 250);
-        
-        // Add the player to our SKView now.
-        addChild(player);
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        // Set player slug sprite in scene
+        buildPlayerSlug();
     }
-    
-    private var touchPos : CGPoint?
-    
     
     func touchDown(atPoint pos : CGPoint) {
         touchPos = pos;
+        /*
         if let n = self.spinnyNode?.copy() as! SKShapeNode? {
             n.position = pos
             n.strokeColor = SKColor.green
             self.addChild(n)
-        }
+        }*/
     }
     
     func touchMoved(toPoint pos : CGPoint) {
         touchPos = pos;
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
+        /*if let label = self.label {
             label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        }*/
         
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
@@ -99,30 +109,52 @@ class GameScene: SKScene{
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
+    var i = 0;
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
         // move the player's position towards x and y direction by 2 everytime touchMoved fires.
-        if(touchPos != nil && touchPos != player.position)
+        if(touchPos != nil)
         {
             let xDiff = touchPos!.x - player.position.x;
             let yDiff = touchPos!.y - player.position.y;
-            let sqrtCounter : CGFloat = 2/(sqrt((xDiff*xDiff)+(yDiff*yDiff)));
-            if(xDiff < -2)
-            {
-                player.position.x += (xDiff * sqrtCounter);
-            }
-            else if(xDiff > 2)
-            {
-                player.position.x += (xDiff * sqrtCounter);
-            }
+            let distDiff : CGFloat = (sqrt((xDiff*xDiff)+(yDiff*yDiff)));
             
-            if(yDiff < -2)
+            if(distDiff > 4)
             {
-                player.position.y += (yDiff * sqrtCounter);
-            }
-            else if(yDiff > 2)
+                // If animation is off, turn it on
+                if(!walkAnimOn)
+                {
+                    animatePlayerSlug();
+                    walkAnimOn = true;
+                }
+                // Move the slug to appropriate position
+                if(xDiff < -2)
+                {
+                    player.position.x += (xDiff * 2/distDiff);
+                    player.xScale = abs(player.xScale);
+                }
+                else if(xDiff > 2)
+                {
+                    player.position.x += (xDiff * 2/distDiff);
+                    player.xScale = abs(player.xScale) * -1;
+                }
+                
+                if(yDiff < -2)
+                {
+                    player.position.y += (yDiff * 2/distDiff);
+                }
+                else if(yDiff > 2)
+                {
+                    player.position.y += (yDiff * 2/distDiff);
+                }
+            }else
             {
-                player.position.y += (yDiff * sqrtCounter);
+                // If animation is on, turn it off
+                if(walkAnimOn)
+                {
+                    player.removeAllActions();
+                    walkAnimOn = false;
+                }
             }
         }
     }
